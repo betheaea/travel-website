924a435306131d0386c8f1e26e0903aaa7d7603d
betheaea <87698730+betheaea@users.noreply.github.com>
2025-08-10 18:39:22 +0100

    Update sub_process.php
    Updated comments for further detail

--- processes/sub_process.php
+++ processes/sub_process.php
@@ -6,7 +6,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=travel_website', 'root', '');
 //Declare how required inputs will be inserted into SQL statement
 $stmt = $pdo->prepare("INSERT INTO subscriptions (email) VALUES (?)");
 
-//Execute SQL statement for processing into the dabatase using 'post' method from contact.php
+//Execute SQL statement for processing into the database using 'post' method from about.php
 $stmt->execute([$_POST['email']]);
 header("Location: ../webpages/thankyousub.php?success=1");
 ?>
